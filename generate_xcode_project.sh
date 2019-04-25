2379dc793ffd91c507f6c3293674718978750386
Niklas Westberg <niklas.westberg@king.com>
2019-04-25 10:19:13 +0200

    Added generate_xcode_project.sh

---
+++ generate_xcode_project.sh
@@ -0,0 +1,7 @@
+cmake . -B.build -G Xcode
+echo ""
+echo "=============================================="
+echo "To open the project in XCode, run:"
+echo "open -na Xcode .build/perf-challenge.xcodeproj"
+echo "=============================================="
+echo ""
